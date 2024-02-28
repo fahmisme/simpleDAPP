f1adb11683c4caccf3f33c83ba5a71fd49a8f1ca
Fahmi Idris <idrisfahmi253@gmail.com>
2024-02-29 02:18:58 +0700

    little update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
